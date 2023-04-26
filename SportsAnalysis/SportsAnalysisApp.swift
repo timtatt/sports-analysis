80c49760c40e2ec8f05b08acc746fd153b8ba68d
Tim Tattersall <tim@tattsy.com>
2023-04-26 19:51:47 +1000

    Initial Commit

---
+++ SportsAnalysis/SportsAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsAnalysisApp.swift
+//  SportsAnalysis
+//
+//  Created by Tim Tattersall on 26/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct SportsAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
